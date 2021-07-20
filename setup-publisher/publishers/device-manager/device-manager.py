7a4c2dc00c942fd4cca1a356e4122887d226f2c0
Max <maximiliano.arias@gmail.com>
2021-07-20 17:52:50 +0200

    Device-Manager change topic from hostname to DeviceId

--- setup-publisher/publishers/device-manager/device-manager.py
+++ setup-publisher/publishers/device-manager/device-manager.py
@@ -2,15 +2,11 @@
 import paho.mqtt.client as mqtt
 import datetime
 from pymongo import MongoClient
-import socket
 import os
 import configparser
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import json
-
-# Hostname
-hostname = socket.gethostname()
 
 # Config reading and valuable
 config = configparser.RawConfigParser()
@@ -23,33 +19,36 @@ brokerPort = config.get('broker', 'brokerPort')
 brokerUsername = config.get('broker', 'brokerUsername')
 brokerPassword = config.get('broker', 'brokerPassword')
 
+### DEVICE INFO ##
+deviceId = config.get('publisher', 'brokerPassword')
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    print("subscribing to: "+ str(hostname))
-    client.subscribe([(str(hostname)+"/reboot",1),(str(hostname)+"/restart/dht11",1),(str(hostname)+"/restart/moisture",1),(str(hostname)+"/restart/ultrasonic",1),(str(hostname)+"/stop/dht11",1),(str(hostname)+"/stop/moisture",1),(str(hostname)+"/stop/ultrasonic",1)]) # QoS 0 = At most once, 1 = At least once , 2 = Exactly once
+    print("subscribing to: "+ str(deviceId))
+    client.subscribe([(str(deviceId)+"/reboot",1),(str(deviceId)+"/restart/dht11",1),(str(deviceId)+"/restart/moisture",1),(str(deviceId)+"/restart/ultrasonic",1),(str(deviceId)+"/stop/dht11",1),(str(deviceId)+"/stop/moisture",1),(str(deviceId)+"/stop/ultrasonic",1)]) # QoS 0 = At most once, 1 = At least once , 2 = Exactly once
 
 def on_message(client, userdata, message):
     topic = str(message.topic)
     message = str(message.payload.decode("utf-8"))
     print(topic +' '+ message)
-    if topic == str(hostname)+"/reboot":
+    if topic == str(deviceId)+"/reboot":
         os.system("systemctl reboot")
-    elif topic == str(hostname)+"/restart/dht11":
+    elif topic == str(deviceId)+"/restart/dht11":
         os.system("systemctl restart dht11.service")
-    elif topic == str(hostname)+"/restart/moisture":
+    elif topic == str(deviceId)+"/restart/moisture":
         os.system("systemctl restart moisture.service")
-    elif topic == str(hostname)+"/restart/ultrasonic":
+    elif topic == str(deviceId)+"/restart/ultrasonic":
         os.system("systemctl restart ultrasonic.service")
-    elif topic == str(hostname)+"/stop/dht11":
+    elif topic == str(deviceId)+"/stop/dht11":
         os.system("systemctl stop dht11.service")
-    elif topic == str(hostname)+"/stop/moisture":
+    elif topic == str(deviceId)+"/stop/moisture":
         os.system("systemctl stop moisture.service")
-    elif topic == str(hostname)+"/stop/ultrasonic":
+    elif topic == str(deviceId)+"/stop/ultrasonic":
         os.system("systemctl stop ultrasonic.service")
 
 
 # Initialize the client that should connect to the Mosquitto broker
-client = mqtt.Client(client_id=hostname+"-manager subscriber", protocol=mqtt.MQTTv31)
+client = mqtt.Client(client_id=deviceId+" subscriber", protocol=mqtt.MQTTv31)
 client.username_pw_set(username=brokerUsername,password=brokerPassword)
 client.on_connect = on_connect
 client.on_message = on_message
